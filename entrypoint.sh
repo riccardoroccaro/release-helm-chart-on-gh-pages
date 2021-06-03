433d993cc1ef39dbc1e4c639b91d40e1b0419bc4
Riccardo Roccaro <r0cc4rd0@gmail.com>
2021-06-03 09:42:23 +0200

    Add dest dir and info log

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,56 +1,73 @@
 #!/bin/sh
 
-# Set vars
-src_folder=$1
-dest_branch=$2
+info(){
+    echo -n "[INFO] $1"
+}
 
-# If repo_url has not been set it will be set to 'https://<owner>.github.com/<repo>'
-[ -z "$3" ] && repo_url="https://$(echo $GITHUB_REPOSITORY | cut -d '/' -f 1).github.com/$(echo $GITHUB_REPOSITORY | cut -d '/' -f 2)" || repo_url=$3
+main(){
+    # Set vars
+    info "Setting variables..."
+    src_folder=$1
+    dest_branch=$2
+    dest_folder=$3
 
-# Check src_folder existence
-! [ -d $src_folder ] && echo "src_folder: $src_folder, doesn't exist." && exit 1
+    # If repo_url has not been set it will be set to 'https://<owner>.github.com/<repo>'
+    [ -z "$4" ] && repo_url="https://$(echo $GITHUB_REPOSITORY | cut -d '/' -f 1).github.com/$(echo $GITHUB_REPOSITORY | cut -d '/' -f 2)" || repo_url=$4
+    echo "Done."
 
-# Check dest_branch existence
-check_dest_branch=$(git branch --list -r origin/${dest_branch})
-[ -z "$check_dest_branch" ] && echo "dest_branch: $dest_branch, doesn't exist." && exit 1
+    # Check src_folder existence
+    info "Checking source folder existence..."
+    ! [ -d $src_folder ] && echo "" && echo "[ERROR] src_folder: $src_folder, doesn't exist." && exit 1 || echo "Done successfully."
 
-# Congfigure git
-git config user.name "$GITHUB_ACTOR"
-git config user.email "$GITHUB_ACTOR@users.noreply.github.com"
+    # Check dest_branch existence
+    info "Checking destination branch existence..."
+    check_dest_branch=$(git branch --list -r origin/${dest_branch})
+    [ -z "$check_dest_branch" ] && echo "" && echo "[ERROR] dest_branch: $dest_branch, doesn't exist." && exit 1 || echo "Done successfully."
 
-# Create temp directory for the helm source
-helm_src_temp_dir=$(mktemp -d -t helm_src-XXXXXXXXXX)
+    # Congfigure git
+    info "Configuring git user..."
+    git config user.name "$GITHUB_ACTOR"
+    git config user.email "$GITHUB_ACTOR@users.noreply.github.com"
+    echo "Done."
 
-# Copy the src_folder content into the temp directory
-cp -r $src_folder/* $helm_src_temp_dir
+    # Create temp directory for the helm source
+    info "Creating src and dest temp dirs and copy source content..."
+    helm_src_temp_dir=$(mktemp -d -t helm_src-XXXXXXXXXX)
 
-# Create temp directory for helm dest
-helm_dest_temp_dir=$(mktemp -d -t helm_src-XXXXXXXXXX)
+    # Copy the src_folder content into the temp directory
+    cp -r $src_folder/* $helm_src_temp_dir
 
-# Checkout to dest_branch and copy its content into the temp directory
-git checkout $dest_branch
-cp -r ./* $helm_dest_temp_dir
+    # Create temp directory for helm dest
+    helm_dest_temp_dir=$(mktemp -d -t helm_src-XXXXXXXXXX)
+    echo "Done."
 
-# Create helm package
-repo_dir=$(pwd)
-echo "pwd=$(pwd)"
-echo "GITHUB_WORKSPACE=$GITHUB_WORKSPACE"
+    # Checkout to dest_branch and copy its content into the temp directory
+    info "Checking out to $dest_branch branch and copy its content on corresponding temp dir..."
+    git checkout $dest_branch
+    cp -r ./* $helm_dest_temp_dir
+    echo "Done."
 
-cd $helm_src_temp_dir
-echo "after cd GITHUB_WORKSPACE=$GITHUB_WORKSPACE"
-helm package .
-mkdir $helm_dest_temp_dir
-mv *.tgz $helm_dest_temp_dir
-cd $helm_dest_temp_dir
-ls -l
-helm repo index . --url $repo_url
-ls -l
+    # Create helm package
+    repo_dir=$(pwd)
+    echo "pwd=$(pwd)"
+    echo "GITHUB_WORKSPACE=$GITHUB_WORKSPACE"
 
-# Copy the helm_dest_temp_dir folder content to the repo_dir
-cp -r $helm_dest_temp_dir/* $repo_dir
-cd $repo_dir
+    cd $helm_src_temp_dir
+    echo "after cd GITHUB_WORKSPACE=$GITHUB_WORKSPACE"
+    helm package .
+    mkdir $helm_dest_temp_dir/$dest_folder
+    mv *.tgz $helm_dest_temp_dir/$dest_folder
+    cd $helm_dest_temp_dir
+    helm repo index ./$dest_folder --url $repo_url
 
-# Commit and push the changes
-git add --all
-git commit -m "Create repository from commit $(echo ${GITHUB_SHA} | cut -c1-7) in branch $(echo ${GITHUB_REF#refs/heads/} | tr / _)"
-git push
+    # Copy the helm_dest_temp_dir folder content to the repo_dir
+    cp -r $helm_dest_temp_dir/* $repo_dir
+    cd $repo_dir
+
+    # Commit and push the changes
+    git add --all
+    git commit -m "Create repository from commit $(echo ${GITHUB_SHA} | cut -c1-7) in branch $(echo ${GITHUB_REF#refs/heads/} | tr / _)"
+    git push
+}
+
+main $@
